51f0b300a4ca16537db99ef7c924801119894b6a
slupdru <VyacheslavDryuchin@yandex.ru>
2018-02-22 23:55:56 +0300

    test

--- src/main.js
+++ src/main.js
@@ -124,7 +124,7 @@ let move_on_mosedown = function(e){
         }
     }
   }
-//   ddddddd
+//   dddddddssssssss
   b_canvas.onmouseup = function(){
     document.onmousemove = null;
     b_canvas.onmouseup = null;
